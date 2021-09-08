fe010dc455bd1249f54dc47a58e50cdda9de77dd
Damien <thecsmine@gmail.com>
2021-09-08 18:40:07 +0200

    Update time.js

--- my_modules/time.js
+++ my_modules/time.js
@@ -1,7 +1,7 @@
 const getLogTime = () => {
 	const d = new Date();
-	const h = d.getHours();
-	const m = d.getMinutes();
+	const h = String(d.getHours()).padStart(2, "0");
+	const m = String(d.getMinutes()).padStart(2, "0");
 	const s = String(d.getSeconds()).padStart(2, "0");
 	return `${h}:${m}:${s}`;
 };
